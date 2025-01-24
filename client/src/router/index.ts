1333b862f112cba463a568c774ee8c5a685f34a0
Kim-Lan <me@kim-lan.com>
2025-01-23 20:15:42 -0800

    Add catch all route

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import NotFound from '../components/NotFound.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: "NotFound",
+    component: NotFound
   }
 ]
 
