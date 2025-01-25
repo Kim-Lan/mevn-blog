406487c6b1577f3048776fd9e2e92ebd3f26a9d3
Kim-Lan <me@kim-lan.com>
2025-01-25 03:50:28 -0800

    Install pinia

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,7 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
@@ -15,9 +16,12 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 /* add icons to the library */
-library.add(faPlus, faGithub)
+library.add(faPlus, faGithub);
+
+const pinia = createPinia();
 
 createApp(App)
   .component('font-awesome-icon', FontAwesomeIcon)
   .use(router)
-  .mount('#app')
+  .use(pinia)
+  .mount('#app');
