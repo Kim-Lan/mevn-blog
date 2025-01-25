d20a6c2b819c8159bfffe86c215756bacc0307e9
Kim-Lan <me@kim-lan.com>
2025-01-25 03:27:23 -0800

    Add logic for registering user

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,23 +1,63 @@
+<script setup>
+import { ref } from 'vue'
+
+const isLoading = ref(false);
+
+const username = ref('');
+const email = ref('');
+const password = ref('');
+
+function register() {
+  try {
+    isLoading.value = true;
+    fetch('http://localhost:3001/api/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: username.value,
+        email: email.value,
+        password: password.value,
+      }),
+    });
+  } catch (error) {
+    
+  } finally {
+    isLoading.value = false;
+  }
+}
+</script>
+
 <template>
   <section class="w-104">
-    <form class="flex flex-col gap-4 bg-white p-8 rounded">
+    <form
+      @submit.prevent="register"
+      class="flex flex-col gap-4 bg-white p-8 rounded"
+    >
       <h2 class="text-2xl text-slate-800">Register</h2>
       <input
+        v-model="username"
         type="text"
         name="username"
         placeholder="Username"
+        :disabled="isLoading"
         class="bg-slate-200 p-2 rounded"
       />
       <input
+        v-model="email"
         type="text"
         name="email"
         placeholder="Email"
+        :disabled="isLoading"
         class="bg-slate-200 p-2 rounded"
       />
       <input
+        v-model="password"
         type="password"
         name="password"
         placeholder="Password"
+        :disabled="isLoading"
         class="bg-slate-200 p-2 rounded"
       />
       <button class="bg-blue-400 text-white drop-shadow rounded p-2">Register</button>
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,4 +1,6 @@
 import asyncHandler from 'express-async-handler'
+import { User } from '../models/user.model.js'
+import bcrypt from 'bcrypt';
 
 export const getAllPosts = asyncHandler(async (req, res) => {
   res.json('get all posts');
@@ -13,5 +15,26 @@ export const loginUser = asyncHandler(async (req, res) => {
 });
 
 export const registerUser = asyncHandler(async (req, res) => {
-  res.json('register user');
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    res.status(400).json({ 'error': 'All fields are required'});
+  }
+
+  const existingUser = await User.exists({ username: { '$regex': username, $options: 'i' }});
+  if (existingUser) {
+    res.status(409).json({ 'error': 'Username already taken'});
+  }
+
+  const existingEmail = await User.exists({ email });
+  if(existingEmail) {
+    res.status(409).json({ 'error': 'Email already in use' });
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = await User.create({ username, email , password: hashedPassword });
+
+  res.status(201).json({ 'message': 'Successfully registered' });
 });
