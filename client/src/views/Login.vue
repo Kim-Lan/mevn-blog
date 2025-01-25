1345bb2b26be45a1f32694bb025dd387fa16e70d
Kim-Lan <me@kim-lan.com>
2025-01-24 23:07:33 -0800

    Create Login page

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
-    <h2>Login</h2>
+  <div class="w-96 py-8 px-4">
+    <form class="flex flex-col gap-4">
+      <h2 class="text-2xl">Login</h2>
+      <input
+        name="Username"
+        placeholder="Username"
+        class="bg-slate-200 p-2 rounded"
+      />
+      <input
+        name="Password"
+        placeholder="Password"
+        class="bg-slate-200 p-2 rounded"
+      />
+      <button class="bg-blue-400 text-white drop-shadow rounded p-2">Login</button>
+    </form>
   </div>
 </template>
