ad9738bc9d38a069a64d14e84a9e10558a873219
Kim-Lan <me@kim-lan.com>
2025-01-24 23:04:50 -0800

    Fill in Create page

--- client/src/components/AppHeader.vue
+++ client/src/components/AppHeader.vue
@@ -6,16 +6,16 @@
 
     <div class="flex gap-4 font-bold">
       <router-link :to="{ name: 'create' }">
-        <button class="rounded-lg bg-slate-100 text-slate-600 py-2 px-3">
+        <div class="rounded-lg bg-slate-100 text-slate-600 py-2 px-3">
           <font-awesome-icon :icon="['fas', 'plus']" />
           Create
-        </button>
+        </div>
       </router-link>
-      <div class="flex flex-col justify-center">
-        <router-link :to="{ name: 'login' }" class="p-2">
+      <router-link :to="{ name: 'login' }" class="p-2">
+        <div class="flex flex-col justify-center">
           Login
-        </router-link>
-      </div>
+        </div>
+      </router-link>
     </div>
   </header>
 </template>
--- client/src/views/Create.vue
+++ client/src/views/Create.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    <h2>Create New Post</h2>
+  <div class="w-full md:w-3/4 lg:w-1/2 py-8 px-4">
+    <form class="flex flex-col gap-4">
+      <h2 class="text-2xl">Create New Post</h2>
+      <input
+        name="title"
+        placeholder="Post Title"
+        class="bg-slate-200 p-2 rounded"
+      />
+      <textarea rows="5" class="bg-slate-200 p-2 rounded"></textarea>
+      <button class="bg-blue-400 text-white drop-shadow rounded p-2">Create Post</button>
+    </form>
   </div>
 </template>
