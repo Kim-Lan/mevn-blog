aad953c4110c9b2c377f4c600d3ce553b4850636
Kim-Lan <me@kim-lan.com>
2025-01-25 01:19:39 -0800

    Add styling to individual post page

--- client/src/views/Post.vue
+++ client/src/views/Post.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 import sourceData from '@/data.json'
+import { formatDate } from '../utils/dateUtils'
 
 const props = defineProps<{
   id: number,
@@ -10,12 +11,23 @@ const post = sourceData.posts.find((post) => post.id === props.id);
 </script>
 
 <template>
-  <section v-if="post">
-    <h2>{{ post.title }}</h2>
-    <h3>{{  post.date }}</h3>
-    <img :src="`/images/${post.cover}`" :alt="post.coverAlt" />
-    <div>
-      {{ post.contents }}
+  <section class="w-full md:w-3/4 lg:w-1/2 bg-white border-1 border-slate-200 rounded-lg overflow-hidden">
+    <!-- Cover Photo -->
+    <img :src="`/images/${post.cover}`" :alt="post.coverAlt">
+
+    <div class="px-5 py-4">
+      <h2 class="font-semibold text-2xl text-slate-800">{{ post.title }}</h2>
+      <p class="text-sm text-slate-600 mb-2">
+        @{{ post.author }} &middot; {{ formatDate(post.date) }}
+      </p>
+
+      <p
+        v-for="(paragraph, index) in post.contents.split('\n')"
+        :key="index"
+        class="my-3 text-slate-800"
+      >
+        {{ paragraph }}
+      </p>
     </div>
   </section>
 </template>
